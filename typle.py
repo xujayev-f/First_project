e0c9bec2f5ee838e8f0565582d088c846fa93296
Fazliddinbek <fazliddinbekxujayev@gmail.com>
2025-05-28 22:50:56 +0500

    first commit

---
+++ typle.py
@@ -0,0 +1,21 @@
+# my_tuple = (1, 2, 3)
+# my_list = list(my_tuple)
+# my_list.append(4)
+# my_tuple = tuple(my_list)
+# print(my_tuple)
+
+# my_tuple = (1, 2, 3)
+# for item in my_tuple:
+#     print(item)
+
+# tuple1 = (1, 2, 3) * 3
+# print(tuple1)
+# tuple2 = (4, 5, 6)
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+# thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+
+# x = thistuple.index(8)
+
+# print(x)
