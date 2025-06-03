87721cf5f74d3682e6722498c67691784341b518
Fazliddinbek <fazliddinbekxujayev@gmail.com>
2025-06-03 16:36:52 +0500

    first commit

---
+++ xDa.py
@@ -0,0 +1,37 @@
+# # # ism = input('Ismiz nima: ')
+# # # print('Sizi ismiz' , ism)
+
+# # int = 10  , -1 , 156
+# # float = 10.4 , 50.9
+# # complex = 34j + 0j
+
+
+# print(int(38.9))
+
+# # print(float(156))
+# a = 26.5
+# print(type(a))
+
+# print(5/2)  # / ---> Float
+# print(5//2) # // ---> Int
+# print(5%2) # % ---> Qoldiqini tovib beradi
+# print(3**3) # ** ----> Darajasini aniqledi
+
+
+# import math
+
+# x = 4
+# y = 6
+
+# print(math.pow(x,y))   # pow ------> Darajani topadi
+# print(math.remainder(x,y)) # % shuni vazifasini bajaradi yani qoldiqni topadi
+# print(math.gcd(x,y)) # EKUB
+# print(math.lcm(x,y)) # EKUK
+# print(math.fabs(-235)) # Modul 
+
+# import random
+# print(random.randrange(10,100)) # kiritgan sondan boshlab ikkinchi songacha tahminiy son oladi
+# print(random.randint(20,50)) # Bu ham tasodifiy son kiritadi
+# print(random.random()) # 0 dan 1 gacha tasodifiy son chiqaradi
+# print(random.uniform(10,100))
+
